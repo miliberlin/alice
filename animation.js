562e3aefbb92f2a135f78876ba3b6599df7b226a
miliberlin <michelle.liebheit@posteo.de>
2019-11-01 15:35:16 +0900

    alice size function

--- animation.js
+++ animation.js
@@ -1,6 +1,32 @@
-const alice = document.getElementById("alice-container");
+const alice = document.getElementById("alice");
 
-const drinkShrink = document.getElementById("drink-pink");
+const drinkShrink = document.getElementById("drink-shrink");
 
-const drinkGrow = document.getElementById("drink-blue");
+const drinkGrow = document.getElementById("drink-grow");
 
+
+drinkShrink.addEventListener("click", aliceShrink);
+function aliceShrink() {
+    if (alice.className === "alice-m") {
+        alice.setAttribute("class", "alice-s");
+    }
+    else if (alice.className === "alice-l") {
+        alice.setAttribute("class", "alice-m");
+    }
+    else {
+        return;
+    }
+}
+
+drinkGrow.addEventListener("click", aliceGrow);
+function aliceGrow() {
+    if (alice.className === "alice-m") {
+        alice.setAttribute("class", "alice-l");
+    }
+    else if (alice.className === "alice-s") {
+        alice.setAttribute("class", "alice-m");
+    }
+    else {
+        return;
+    }
+}
